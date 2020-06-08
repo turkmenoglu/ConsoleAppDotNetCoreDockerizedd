f0794ee5aea1468b2ea58f677244bb32824092dd
Turkmenoglu, Ismail <Ismail.Turkmenoglu@adesso.com.tr>
2020-06-08 22:16:43 +0300

    first commit

---
+++ ConsoleAppDotNetCoreDockerizedd/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsoleAppDotNetCoreDockerizedd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
